a17c0900bf7d929bced0e8b230ca0ed7107d80d8
Abolfazl Rashidian <abolf4zlrashidian@gmail.com>
2025-08-02 01:12:53 +0330

    Athlete card button became responsive

--- app.py
+++ app.py
@@ -344,4 +344,4 @@ def history():
                          search_query=search_query, action_type=action_type)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
